7d2c1e2028ebb4f2599bac0035ed212b302f9d4a
pangduner <sxqgool@qq.com>
2020-03-23 21:40:15 +0800

    project init

---
+++ src/App.vue
@@ -0,0 +1,31 @@
+<template>
+  <div id="app">
+    <el-container style="height: 500px; border: 1px solid #eee">
+  <el-aside width="200px" style="background-color: rgb(238, 241, 246)">
+  </el-aside>
+  <el-container>
+    <el-header style="text-align: right; font-size: 12px">
+    </el-header>
+    <el-main>
+      <router-view/>
+    </el-main>
+  </el-container>
+</el-container>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App'
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+</style>
