1f83151887de7daff66fba37c4a39a69ac01f871
孙晓强 <sunxiaoqiang@bonc.com.cn>
2020-03-27 13:28:04 +0800

    加入less

--- src/App.vue
+++ src/App.vue
@@ -1,31 +1,35 @@
 <template>
   <div id="app">
-    <el-container style="height: 500px; border: 1px solid #eee">
-  <el-aside width="200px" style="background-color: rgb(238, 241, 246)">
-  </el-aside>
-  <el-container>
-    <el-header style="text-align: right; font-size: 12px">
-    </el-header>
-    <el-main>
-      <router-view/>
-    </el-main>
-  </el-container>
-</el-container>
+    <el-container class="box1">
+      <el-aside
+        width="200px"
+        style="background-color: rgb(238, 241, 246)"
+      ></el-aside>
+      <el-container>
+        <el-header style="text-align: right; font-size: 12px;background-color:rgb(238, 241, 246);"></el-header>
+        <el-main style="background-color:rgb(238, 241, 246);">
+          <div style="background-color:#ffffff;height:30px;margin: 0 auto; padding:20px 10px 0px 0px;">
+            <el-breadcrumb separator-class="el-icon-arrow-right">
+              <el-breadcrumb-item :to="{ path: '/' }">首页</el-breadcrumb-item>
+              <el-breadcrumb-item>活动管理</el-breadcrumb-item>
+              <el-breadcrumb-item>活动列表</el-breadcrumb-item>
+              <el-breadcrumb-item>活动详情</el-breadcrumb-item>
+            </el-breadcrumb>
+          </div>
+          <router-view />
+        </el-main>
+      </el-container>
+    </el-container>
   </div>
 </template>
 
 <script>
+import './App.less'
 export default {
   name: 'App'
 }
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
+
 </style>
