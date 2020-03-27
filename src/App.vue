6140ee55dbc91637df6b1cdf500949d38833df97
孙晓强 <sunxiaoqiang@bonc.com.cn>
2020-03-27 13:57:53 +0800

    样式

--- src/App.vue
+++ src/App.vue
@@ -3,12 +3,13 @@
     <el-container class="box1">
       <el-aside
         width="200px"
-        style="background-color: rgb(238, 241, 246)"
-      ></el-aside>
+        class="box"
+      >
+      </el-aside>
       <el-container>
-        <el-header style="text-align: right; font-size: 12px;background-color:rgb(238, 241, 246);"></el-header>
-        <el-main style="background-color:rgb(238, 241, 246);">
-          <div style="background-color:#ffffff;height:30px;margin: 0 auto; padding:20px 10px 0px 0px;">
+        <el-header class="header-box"></el-header>
+        <el-main class="box">
+          <div class="title-box">
             <el-breadcrumb separator-class="el-icon-arrow-right">
               <el-breadcrumb-item :to="{ path: '/' }">首页</el-breadcrumb-item>
               <el-breadcrumb-item>活动管理</el-breadcrumb-item>
@@ -31,5 +32,4 @@ export default {
 </script>
 
 <style>
-
 </style>
