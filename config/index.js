69fca6fad1f8f06d2763902ab772bbb60332910e
孙晓强 <sunxiaoqiang@bonc.com.cn>
2020-03-31 15:19:00 +0800

    代理

--- config/index.js
+++ config/index.js
@@ -12,8 +12,9 @@ module.exports = {
     assetsPublicPath: '/',
     proxyTable: {
       '/api': {
-        target: 'http://localhost:8080',
-        changeOrigin: true,
+        'target': 'http://127.0.0.1:8080/',
+        'ws':true,
+        'changeOrigin':true,
         pathRewrite: {
           '^/api': '/'
         }
@@ -22,8 +23,8 @@ module.exports = {
     },
 
     // Various Dev Server settings
-    host: 'localhost', // can be overwritten by process.env.HOST
-    port: 8080, // can be overwritten by process.env.PORT, if port is in use, a free one will be determined
+    host: '127.0.0.1', // can be overwritten by process.env.HOST
+    port: 8081, // can be overwritten by process.env.PORT, if port is in use, a free one will be determined
     autoOpenBrowser: false,
     errorOverlay: true,
     notifyOnErrors: true,
